4b03c1b4de69911085de8d7031837f7f063b4cdf
diegocolombo1989 <57449668+diegocolombo1989@users.noreply.github.com>
2019-11-11 11:33:26 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,16 @@
+idade = 30
+salario = 490.00
+nome = 'Diego'
+verdadeiro = True
+falso  = False 
+
+print('-'*50, '\n'*2)
+
+print('idade:', idade, 'salario:', salario, 'nome:', 'verdadeiro:', verdadeiro, 'falso:', falso)
+print('idade:{} salario:{} nome:{}verdadeiro:{}falso:{}'.format(idade, salario, nome, verdadeiro, falso))
+print(f'idade:{idade} salario:{salario} nome:{nome} verdadeiro:{verdadeiro} falso:{falso}')
+
+
+
+
+print('\n'*2, '-'*50)
